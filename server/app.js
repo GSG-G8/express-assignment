f22e717b3de8db44a50c4390c0ae37d1fe909c21
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-02-22 08:42:46 +0200

    relates #28

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const router = require('./routes');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
 app.set('port', port);
